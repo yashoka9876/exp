b00dcd6049302d18d6edefda2847cfb44bcb2f03
AshokYadav <yashoka51@gmail.com>
2024-02-22 18:17:31 +0530

    adding the concept of password change handler

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,6 +1,8 @@
-import React, { useRef} from 'react'
+import React, { useRef, useState} from 'react'
 
 const Authentication = ({isLogin,mode,SignUp,ModeHandler}) => {
+    const[forgetpass,setForgetpass]=useState(false);
+    const[isLoading,setIsLoading]=useState(false);
     const Email=useRef();
     const Password=useRef();
     const ConfirmPassword=useRef();
@@ -28,6 +30,39 @@ const Authentication = ({isLogin,mode,SignUp,ModeHandler}) => {
             Password.current.value='';
             
         }
+
+        function forgetPasshandler(){
+            console.log(forgetpass)
+            setForgetpass((value)=>!value);
+            async function forgetPasswordHandler(){
+                setIsLoading(true)
+                try{const response=await fetch('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDa_jbUsg5x1ywvKesSXurNjxjYY7Hn2BU',{
+                    method:'POST',
+                    headers:{
+                        'Content-Type':'application/json'
+                    },
+                    body:JSON.stringify({
+                        requestType: "PASSWORD_RESET" ,
+                        email:Email.current.value
+                    })
+                })
+                if(!response.ok){
+                   
+                    throw new Error('didn\'t reset bro! ')
+                }
+                setIsLoading(false)
+                const data=await response.json();
+                console.log(data);
+            }catch(error){
+                setIsLoading(false)
+                console.error(error);
+            }
+            }
+            console.log(forgetpass)
+            if(forgetpass){
+                forgetPasswordHandler();
+            }
+        }
     
 
 
@@ -43,22 +78,26 @@ const Authentication = ({isLogin,mode,SignUp,ModeHandler}) => {
        {isLogin && <div>welcome to the the expense tracker</div>}
         <h1 className='mb-4'>{mode==='login'?'Login':'SignUp'}</h1>
         <form onSubmit={submitHandler} className="row g-3 border border-black border-4  w-25 rounded-3" >
-            
+            {isLoading && <h1>password is resseting</h1>}
             <div className='form-group'>
                 <label htmlFor="staticEmail2"  >Email</label>
                 <input ref={Email} type="text" className='form-control' id="staticEmail2" required/>
             </div>
-            <div >
+           {!forgetpass && <div >
                 <label htmlFor="inputPassword2" >Password</label>
                 <input ref={Password} type="password" className="form-control" id="inputPassword2" required />
-            </div>
+            </div>}
            {mode==='SignUp' && <div >
                 <label htmlFor="confirmPassword" >ConfirmPassword</label>
                 <input ref={ConfirmPassword} type="password" className="form-control" id="confirmPassword" required />
             </div>}
+            <div className=' d-flex  flex-column justify-content-center'>
+                 <button  type='button' className='btn btn-primary m-1 ' onClick={forgetPasshandler}>{!forgetpass ? 'Forget Password':'sendLink'}</button>
+                {forgetpass && <button type='button' className='btn btn-primary ' onClick={forgetPasshandler}>already a user? login</button>}
+            </div>
             <div className='d-flex justify-content-center '>
-                <button  type="submit" className="btn btn-primary mb-3 hover active">{mode==='login'?'Login':'SignUp'}</button>
-                <button onClick={ModeHandler} type='button' className='btn btn-primary mb-3 hover active mx-1'>change mode </button>
+                {!forgetpass && <button  type="submit" className="btn btn-primary mb-3 hover active">{mode==='login'?'Login':'SignUp'}</button>}
+                {!forgetpass && <button onClick={ModeHandler} type='button' className='btn btn-primary mb-3 hover active mx-1'>change mode </button>}
             </div>
         </form>
     </div>
